39e739beca524d351e132559f21519276a79d264
Antonio Nardini <66033717+NardiniA@users.noreply.github.com>
2023-02-13 21:06:20 +0000

    Initial commit

---
+++ src/collections/Orders/access/adminsAndOrderedBy.ts
@@ -0,0 +1,16 @@
+import type { Access } from 'payload/config'
+import { checkRole } from '../../Users/checkRole'
+
+export const adminsAndOrderedBy: Access = ({ req: { user } }) => {
+  if (user) {
+    if (checkRole(['admin'], user)) {
+      return true
+    }
+
+    return {
+      'orderedBy.user.id': user.id,
+    }
+  }
+
+  return true
+}
